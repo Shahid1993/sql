0d7faacbb31a87fb21d710a2537e768369492ea0
Shahid1993 <31109285+Shahid1993@users.noreply.github.com>
2019-03-25 11:26:28 +0530

    Update 01_UpdateColWithDiffValsBasedOnCond.sql

--- 01_UpdateColWithDiffValsBasedOnCond.sql
+++ 01_UpdateColWithDiffValsBasedOnCond.sql
@@ -1,3 +1,16 @@
+UPDATE table_users
+    SET cod_user = (case when user_role = 'student' then '622057'
+                         when user_role = 'assistant' then '2913659'
+                         when user_role = 'admin' then '6160230'
+                    end),
+        date = '12082014'
+    WHERE user_role in ('student', 'assistant', 'admin') AND
+          cod_office = '17389551';
+
+
+
+
+
 update express_address 
 set is_default = (case when addr_id = 16 then true else false end),
 modified_by = 1411, modified_date = CURRENT_DATE
